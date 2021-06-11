5c33619c4eddc42f740abb707e7c8e53e02711a9
Patrick Charron-Morneau <patrick.cmorneau@cidco.ca>
2021-06-11 11:46:57 -0400

    xml format + premade query

--- src/get_areas.py
+++ src/get_areas.py
@@ -14,15 +14,38 @@ class areas:
 
 	
 	def __init__(self,query):
-		self.query = query
+		if query == "trap" :
+			query = "SELECT * FROM crabnet.dfo_engins WHERE type='CASIER/CAGE - TRAP/POT'"
+			self.query = query
+		else:
+			print("error")
 		
 	
 	def get_data(self):
 		cursor = areas.db.cursor()
 		cursor.execute(self.query)
 		result = cursor.fetchall()
+		gpx = gpxpy.gpx.GPX()
+		gpx.name = 'Casiers perdus'
+		gpx.description = 'Casiers de peche au crabe perdus'
+
+		# Pour tous les casiers rapportes
+		for trap in result:
+			longitude = trap[6]
+			latitude  = trap[7]
+
+			# TODO: exclude, and see if in zone
+			waypoint = gpxpy.gpx.GPXWaypoint()
+			waypoint.longitude = longitude
+			waypoint.latitude  = latitude
+			#waypoint.symbol    = 'Marks-Mooring-Float'
+			waypoint.name      = "Casier {}".format(trap[0])
+			waypoint.description = trap[2]
+			gpx.waypoints.append(waypoint)
+		result = gpx.to_xml()
+			
 		return result
 
-test = areas("SELECT * FROM crabnet.dfo_engins WHERE type='CASIER/CAGE - TRAP/POT'")
+test = areas("trap")
 data = test.get_data()
 print(data)
