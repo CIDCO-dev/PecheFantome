c30c7475aec06f03f13c9064895e9f7f3f4ea907
patrick charron morneau <patcmorneau@gmail.com>
2021-11-18 18:14:08 -0500

    create a directory per dataset ?

--- src/GIS/mammals_distribution.py
+++ src/GIS/mammals_distribution.py
@@ -4,22 +4,26 @@ import tqdm
 
 
 
-def extract_contour(inputFilePath, outputPath):
+def extract_contour(inputFilePath, outputFilePath):
 	gdal_contour = "/usr/bin/gdal_contour"
-	outputPath = inputFilePath[:-4]
-	export = subprocess.Popen([gdal_contour, "-a", "sightings", "-i", "1", "-f", "ESRI Shapefile", inputFilePath, outputPath])
+	if not os.path.exists(inputFilePath):
+		print("input File Path does not exist")
+		return false
+	export = subprocess.Popen([gdal_contour, "-a", "sightings", "-i", "1", "-f", "ESRI Shapefile", inputFilePath, outputFilePath])
 	export.wait()
 	return True
 
 
-"""
+
 inputFilePath = "mammal/KD2.tif"
-outputPath = "../"
+outputPath = "../kd2.shp"
 
 status = extract_contour(inputFilePath,outputPath)
 if(status == True):
 	print("done")
-"""
+
 
 def export2DB():
 	#ogr2ogr -f "MySQL"   MYSQL:"crabnet,host=localhost,user=admin,password=jmlespatate,port=3306" -a_srs "EPSG:4326" ‘/home/pat/PecheFantome/data/Zones Protegees MPO/DFO_MPA_MPO_ZPM.shp’
+	pass
+
